d60edf6e40117e14123c8be3a04d934b080ec54f
sara <sara27082011@gmail.com>
2021-12-18 10:06:30 -0300

    init project

--- django_com_python/produto/views.py
+++ django_com_python/produto/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+# Create your views here.
+
+def metodo(request):
+    return render(request, 'produto/home.html')
 # Create your views here.
