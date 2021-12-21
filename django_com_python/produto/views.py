a20a874ae99f6dfa1be7b780f4d01a46e5fa677c
sara <sara27082011@gmail.com>
2021-12-21 19:23:40 -0300

    remove folder django_com_python

--- django_com_python/produto/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-def metodo(request):
-    return render(request, 'produto/home.html')
-# Create your views here.
