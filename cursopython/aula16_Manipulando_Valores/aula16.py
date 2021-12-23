023f1c711898aba72ac451da03b44b7fa57d0a12
sara <sara27082011@gmail.com>
2021-12-23 19:17:23 -0300

    rm django

---
+++ cursopython/aula16_Manipulando_Valores/aula16.py
@@ -0,0 +1,17 @@
+"""
+:s - Texto (strings)
+:d - Inteiros
+:f - float
+:.(número)f - Quantidade de casas decimais após a vírgula
+:(caractere)(> ou < ou ^)(Quantidade)(Tipo - s, d ou f)
+
+> - Esquerda
+< - Direita
+^ - Centro
+"""
+
+nome = 'Sara tavares'
+num = 22
+
+print(nome)
+print(f'{nome:*^20}'.title())
